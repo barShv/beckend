f887a172966ce32248329e266f1119ff9cc4b98a
בר שוורצמן <you@example.com>
2021-12-22 17:12:45 +0200

    assignment9

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = 'bar555'
+
+# define a users dict
+users = {'user1': {'name': 'Bar', 'email': 'bar666@gmail.com'},
+        'user2': {'name': 'Gal', 'email': 'galgol3@gmail.com'},
+        'user3': {'name': 'Roni', 'email': 'roniS7@gmail.com'},
+        'user4': {'name': 'Ziv', 'email': 'zivi6@gmail.com'},
+        'user5': {'name': 'Yonit', 'email': 'yonitnit@gmail.com'},
+        'user6': {'name': 'Eyal', 'email': 'Eyali5@gmail.com'}}
+
+def check_email (email_to_check):
+    for user_num, user_info in users.items():
+        if (user_info['email'] == email_to_check):
+            return (user_info['name'], user_info['email'])
+    return False
 
 
 @app.route('/cvMain')
@@ -15,10 +30,28 @@ def assignment8():
                            languages=['english', 'hebrew'])
 
 
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    if request.method == 'GET':
+        if 'user_email' in request.args:
+            if (check_email(request.args['user_email']) != False):
+                user_name = check_email(request.args['user_email'])[0]
+                user_email = check_email(request.args['user_email'])[1]
+                return render_template('assignment9.html', user_name=user_name, user_email=user_email)
+        return render_template('assignment9.html', users=users)
+    if request.method == 'POST':
+        userName = request.form['userName']
+        session['userName'] = userName
+    return render_template('assignment9.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def log_out():
+    session['userName'] = ""
+    return redirect(url_for('assignment9'))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)    # start run the server
-
-
+    app.run(debug=True)  # start run the server
 
 '''
 from assigment 7:
